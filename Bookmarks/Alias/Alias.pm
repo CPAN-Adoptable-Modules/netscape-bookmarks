58de35723286a49921c93f899f20d7d5c7a7541e
brian d foy <brian.d.foy@gmail.com>
2001-08-08 18:03:31 +0000

    Initial revision

---
+++ Bookmarks/Alias/Alias.pm
@@ -0,0 +1,148 @@
+package Netscape::Bookmarks::Alias;
+# $Revision: 1.1 $
+# $Id: Alias.pm,v 1.1 2001/08/08 18:03:32 comdog Exp $
+
+=head1 NAME
+
+Netscape::Bookmarks::Alias - object for an Alias in a Netscape Bookmarks file
+
+=head1 SYNOPSIS
+
+	use Netscape::Bookmarks;
+	use Netscape::Bookmarks::Alias;
+
+	my $bookmarks = Netscape::Bookmarks->new();
+
+	my $alias = Netscape::Bookmarks::Alias->new();
+
+	$bookmarks->add( $alias );
+	# ... and other Netscape::Bookmark::Category methods
+
+=head1 DESCRIPTION
+
+This module provides an abstraction for an Alias object in a Netscape
+Bookmarks file. An alias is simply a reference to another link in the
+Bookmarks file, henceforth called the target. If you change the alias, 
+the target link also changes.
+
+=over 4
+
+=cut
+
+use strict;
+
+use subs qw();
+use vars qw($VERSION $ERROR @EXPORT @EXPORT_OK @ISA %aliases);
+
+use Exporter;
+
+use Netscape::Bookmarks::Link;
+
+($VERSION) = q$Revision: 1.1 $ =~ m/(\d+\.\d+)\s*$/;
+
+@EXPORT    = qw();
+@EXPORT_OK = qw();
+@ISA       = qw();
+
+=item new( ALIASID )
+
+=cut
+
+sub new
+	{
+	my $class  = shift;
+	my $param  = shift;
+	
+	my $self = {};
+			
+	bless $self, $class;
+	
+	$self->{'alias_of'} = $param;
+			
+	$self;
+	}
+
+=item $obj->alias_of()
+
+Returns the alias key for this alias
+
+=cut
+
+sub alias_of
+	{
+	my $self = shift;
+	
+	return $self->{'alias_of'};
+	}
+
+=item $obj->target( ALIAS_KEY )
+
+Returns the target Link of the given alias key.  The return value
+is a C<Netscape::Bookmarks::Link> object if the target exists, or
+undef in scalar context or the empty list in list context if the 
+target does not exist. If you want to simply check to see if a 
+target exists, use C<target_exists>.
+
+=cut
+
+sub target
+	{
+	my $self     = shift;
+	
+	return $aliases{$self->{'alias_of'}};
+	}
+
+=item add_target( $link_obj, ALIAS_KEY )
+
+Adds a target link for the given ALIAS_KEY. You can add target
+links before the Alias is created.
+
+=cut
+
+# this should really be in Link.pm right?	
+sub add_target
+	{
+	my $target   = shift; #link reference
+	my $alias_id = shift;
+	
+	($$target)->aliasid($alias_id);
+	$aliases{$alias_id} = $$target;
+	}
+
+=item target_exists( TARGET_KEY )
+
+For the given target key returns TRUE or FALSE if the target
+exists.
+
+=cut
+
+sub target_exists
+	{
+	my $target = shift;
+	
+	exists $aliases{$target} ? 1 : 0;
+	}	
+	
+"if you want to believe everything you read, so be it.";
+
+=back
+
+=head1 AUTHOR
+
+brian d foy E<lt>comdog@panix.comE<gt>
+
+=head1 COPYRIGHT
+
+This program is free software; you can redistribute it
+and/or modify it under the same terms as Perl itself.
+
+If you send me modifications or new features, I will do
+my best to incorporate them into future versions.
+
+=head1 SEE ALSO
+
+L<Netscape::Bookmarks>
+
+=cut
+
+__END__
---
+++ Bookmarks/Separator/Separator.pm
@@ -0,0 +1,100 @@
+package Netscape::Bookmarks::Separator;
+# $Revision: 1.1 $
+# $Id: Separator.pm,v 1.1 2001/08/08 18:03:32 comdog Exp $
+
+=head1 NAME
+
+Netscape::Bookmarks::Separator	- manipulate, or create Netscape Bookmarks files
+
+=head1 SYNOPSIS
+
+	use Netscape::Bookmarks::Category;
+	use Netscape::Bookmarks::Separator;
+  
+	#add a separator to a category listing
+	my $category  = new Netscape::Bookmarks::Category { ... };
+	my $separator = new Netscape::Bookmarks::Separator;
+	my $category->add($separator);
+  
+	#print the separator
+	#note that Netscape::Category::as_string does this for you
+	print $separator->as_string;
+
+=head1 DESCRIPTION
+
+Store a Netscape bookmark separator object.
+
+=head1 METHODS
+
+=over 4
+
+=cut
+
+use strict;
+
+use subs qw();
+use vars qw($VERSION $ERROR @EXPORT @EXPORT_OK @ISA);
+
+use Exporter;
+
+use URI::URL;
+
+($VERSION) = q$Revision: 1.1 $ =~ m/(\d+\.\d+)\s*$/;
+
+@EXPORT    = qw();
+@EXPORT_OK = qw();
+@ISA       = qw();
+
+=head2 Netscape::Bookmarks::Separator->new
+
+Creates a new Separator object.  This method takes no arguments.
+
+=cut
+
+sub new
+	{
+	my $class  = shift;
+	
+	my $n = '';
+	my $self = \$n;
+	
+	bless $self, $class;
+				
+	$self;
+	}
+	
+=head2 $obj->as_string
+
+Prints the separator object in the Netscape bookmark format.  One should
+not have to do this as Netscape::Bookmarks::Category will take care of it.
+
+=cut
+
+sub as_string
+	{
+	return "<HR>";
+	}
+
+"if you want to believe everything you read, so be it."
+
+__END__
+
+=back
+
+=head1 AUTHOR
+
+brian d foy E<lt>comdog@panix.comE<gt>
+
+=head1 COPYRIGHT
+
+This program is free software; you can redistribute it
+and/or modify it under the same terms as Perl itself.
+
+If you send me modifications or new features, I will do
+my best to incorporate them into future versions.
+
+=head1 SEE ALSO
+
+L<Netscape::Bookmarks>
+
+=cut
